f68a1bd422488239f1b62997699035d05ae4112d
khalil ben aissa <khalilbenaissa10@gmail.com>
2017-04-28 15:24:25 +0100

    cors update to post

--- src/lib/app.js
+++ src/lib/app.js
@@ -7,12 +7,13 @@ require('./parser')(app);
 var actors = require('../routes/actors');
 var movies = require('../routes/movies');
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+// Cross Origin middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 // Actors routes
 app.route('/actors')
