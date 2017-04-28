fdd89aceb1633b8b2f8cef2e887615d8fcb91104
khalil ben aissa <khalilbenaissa10@gmail.com>
2017-04-28 14:18:08 +0100

    cors

--- src/lib/app.js
+++ src/lib/app.js
@@ -7,6 +7,11 @@ require('./parser')(app);
 var actors = require('../routes/actors');
 var movies = require('../routes/movies');
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // Actors routes
 app.route('/actors')
