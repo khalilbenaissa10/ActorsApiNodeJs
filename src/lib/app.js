5aedacaf6921964a4816dbec35d8f10ba47dfb80
khalil ben aissa <khalilbenaissa10@gmail.com>
2017-04-28 15:17:19 +0100

    cors methods

--- src/lib/app.js
+++ src/lib/app.js
@@ -9,6 +9,7 @@ var movies = require('../routes/movies');
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
